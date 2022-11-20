73bc70eef7d640843dd7b9d2d8c89232f0efb997
Kantapon Srigadphach <kantapon.sri@student.mahidol.ac.th>
2022-11-20 16:47:31 +0700

    update

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -10,8 +12,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class MapParserTest
-{
+public class MapParserTest {
     @Mock
     private BoardFactory boardFactory;
     @Mock
@@ -22,8 +23,8 @@ public class MapParserTest
     @Test
     public void testParseMapGood() {
         MockitoAnnotations.initMocks(this);
-        assertNotNull (boardFactory);
-        assertNotNull (levelFactory);
+        assertNotNull(boardFactory);
+        assertNotNull(levelFactory);
         Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
         MapParser mapParser = new MapParser(levelFactory, boardFactory);
         ArrayList<String> map = new ArrayList<>();
@@ -32,6 +33,28 @@ public class MapParserTest
         map.add("############");
         mapParser.parseMap(map);
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
+        Mockito.verify(boardFactory, Mockito.times(1)).createBoard(Mockito.any());
+        Mockito.verify(levelFactory, Mockito.times(1)).createLevel(Mockito.any(), Mockito.any(), Mockito.any());
+    }
+
+    /**
+     * Test for the parseMap method (bad map).
+     */
+    @Test
+    public void testParseMapWrong1() {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+                MockitoAnnotations.initMocks(this);
+                assertNotNull(boardFactory);
+                assertNotNull(levelFactory);
+                MapParser mapParser = new MapParser(levelFactory, boardFactory);
+                ArrayList<String> map = new ArrayList<>();
+                map.add("############");
+                map.add("#P        X#");
+                map.add("############");
+                mapParser.parseMap(map);
+            });
+        Assertions.assertEquals("Invalid character at 10,1: X", thrown.getMessage());
     }
 }
 
